8b0ba2c1a5fe36b4606bfeaa519845a69d765ff1
Munticaro <diskor1996@gmail.com>
2023-06-24 02:20:22 +0700

    refactor Pages hw5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -15,21 +15,29 @@ function Pages() {
     return (
         <div>
             {/*Routes выбирает первый подходящий роут*/}
+            {/*роутинг будут писать студенты*/}
+            {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+            {/*<Route ...*/}
+
+            {/*роуты для /pre-junior, /junior, /junior-plus*/}
+            {/*<Route ...*/}
+            {/*<Route ...*/}
+            {/*<Route ...*/}
+
+            {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+            {/*<Route ...*/}
             <Routes>
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+                <Route path={'/'} element={<Navigate to={'/pre-junior'}/>}/>
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                <Route path={'/pre-junior'} element={<PreJunior/>}/>
+                <Route path={'/junior'} element={<Junior/>}/>
+                <Route path={'/junior-plus'} element={<JuniorPlus/>}/>
 
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                <Route path={'/*'} element={<Error404/>}/>
             </Routes>
         </div>
     )
 }
+
 
 export default Pages
