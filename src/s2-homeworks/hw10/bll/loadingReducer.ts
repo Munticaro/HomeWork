36055dc1a5ee50cfbbc40bd3454c2de949621085
Munticaro <diskor1996@gmail.com>
2023-12-13 20:03:08 +0300

    completed task hw10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,16 +2,20 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): any => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
-
+        case "CHANGE_LOADING": {
+            return {
+                ...state,
+                isLoading: true,
+            };
+        }
         default:
-            return state
+            return state;
     }
 }
 
-type LoadingActionType = {
+export type LoadingActionType = {
     type: 'CHANGE_LOADING'
     isLoading: boolean
 }
