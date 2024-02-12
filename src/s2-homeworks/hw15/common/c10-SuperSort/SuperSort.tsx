a0ab1edf621220f24605911d37c7494cafa3d316
Munticaro <diskor1996@gmail.com>
2024-02-12 14:35:19 +0300

    completed task hw15

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -16,7 +16,7 @@ export type SuperSortPropsType = {
 }
 
 export const pureChange = (sort: string, down: string, up: string) => {
-    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
+    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down...
     if (sort === down) {
         return up
     } else if (sort === up) {
